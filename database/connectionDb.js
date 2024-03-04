f9f688eaad686e28668a5d846b04e068913ab29a
ArlintonAr <arlintonarturodelgadoflores@gmail.com>
2024-03-04 13:44:36 -0500

    update

--- database/connectionDb.js
+++ database/connectionDb.js
@@ -5,7 +5,8 @@ const connectionDb = async () => {
 
 
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.USERDB}:${process.env.PASSWORDDB}@cluster0.0kexukl.mongodb.net/siteWebDB`)
+        await mongoose.connect
+        (`mongodb+srv://${process.env.USERDB}:${process.env.PASSWORDDB}@cluster0.0kexukl.mongodb.net/siteWebDB`)
             .then(() => console.log('Base de datos conectada!'))
     } catch (error) {
         throw Error("Ha ocurrido un error en la conexión:", error)
