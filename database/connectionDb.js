f98536b9b2b3ad8f6d7f2bd49653985df1b386c0
ArlintonAr <arlintonarturodelgadoflores@gmail.com>
2024-03-04 13:37:18 -0500

    update

--- database/connectionDb.js
+++ database/connectionDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectionDb = async () => {
 
-    const dbLocal='mongodb://localhost:27017/siteWebDB'
+
     try {
         await mongoose.connect(`mongodb+srv://${process.env.USERDB}:${process.env.PASSWORDDB}@cluster0.0kexukl.mongodb.net/siteWebDB`)
             .then(() => console.log('Base de datos conectada!'))
